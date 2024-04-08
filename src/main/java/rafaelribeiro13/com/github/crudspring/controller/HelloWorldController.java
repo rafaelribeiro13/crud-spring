8074d748229e11a2ff6ed8eeeeb0a39938029e59
Rafael Ribeiro <rafaelsribeiro45@gmail.com>
2024-04-08 20:01:06 -0300

    commit inicial

---
+++ src/main/java/rafaelribeiro13/com/github/crudspring/controller/HelloWorldController.java
@@ -0,0 +1,16 @@
+package rafaelribeiro13.com.github.crudspring.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/hello")
+public class HelloWorldController {
+
+    @GetMapping()
+    public String hello() {
+        return "Hello, World!";
+    }
+    
+}
