f1e8995144f761663e75cd15c9b2f5709318513a
Rafael Ribeiro <rafaelsribeiro45@gmail.com>
2024-04-13 16:13:31 -0300

    criado endpoint para excluir cursos

--- src/main/java/rafaelribeiro13/com/github/crudspring/controller/CourseController.java
+++ src/main/java/rafaelribeiro13/com/github/crudspring/controller/CourseController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -54,6 +55,17 @@ public class CourseController {
 
                 Course updated = repository.save(courseFound);
                 return ResponseEntity.ok().body(updated);
+            })
+            .orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        return repository
+            .findById(id)
+            .map(course -> {
+                repository.delete(course);
+                return ResponseEntity.noContent().<Void>build();
             })
             .orElse(ResponseEntity.notFound().build());
     }
