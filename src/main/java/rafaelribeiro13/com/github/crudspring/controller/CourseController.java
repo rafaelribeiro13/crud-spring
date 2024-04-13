d1711d851dabd71a0ed95f853f2177b91b29355b
Rafael Ribeiro <rafaelsribeiro45@gmail.com>
2024-04-12 21:42:07 -0300

    implementado endpoint para obter curso por id

--- src/main/java/rafaelribeiro13/com/github/crudspring/controller/CourseController.java
+++ src/main/java/rafaelribeiro13/com/github/crudspring/controller/CourseController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,6 +27,14 @@ public class CourseController {
     @GetMapping
     public ResponseEntity<List<Course>> findAll() {
         return ResponseEntity.status(HttpStatus.OK).body(repository.findAll());
+    }
+
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<Course> findById(@PathVariable Long id) {
+       return repository
+            .findById(id)
+            .map(course -> ResponseEntity.ok().body(course))
+            .orElse(ResponseEntity.notFound().build());
     }
 
     @PostMapping
